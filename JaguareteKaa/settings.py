f4ad909616873889bb11270c60e04ab702b4af17
Braian Gonzales <braian.gonzales77@gmail.com>
2021-11-25 18:52:53 -0300

    Update settings.py

--- JaguareteKaa/settings.py
+++ JaguareteKaa/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -82,7 +83,8 @@ WSGI_APPLICATION = 'JaguareteKaa.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3', # Comenzó a dar alertas en consola al iniciar la app
+        'NAME': str(os.path.join(BASE_DIR, "db.sqlite3"))
     }
 }
 
